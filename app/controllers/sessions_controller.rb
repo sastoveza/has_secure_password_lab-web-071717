22f370c2b7a6fb5e08eeb913c706c9da6432dbbf
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-21 13:45:18 -0400

    done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+
+	def create 
+		@user = User.find_by(name: params[:user][:name])
+		return head(:forbidden) unless @user.authenticate(params[:user][:password])
+		session[:user_id] = @user.id
+
+		redirect_to root_path
+	end
+end
